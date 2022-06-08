52763ccbd55d119365172657c9bcd07e9f8a4e07
0x616c <mial.burg@gmail.com>
2022-06-08 12:14:14 +0200

    compatibility update

--- src/config.h
+++ src/config.h
@@ -1,17 +1,24 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#if defined(_WIN32) || defined(WIN32)
+  #define OS_WINDOWS
+#elif defined(__unix__)
+  #define OS_UNIX
+#endif
 
-/* Define if we have the stat function */
-#define HAVE_STAT 1
-
-#define HAVE_UNISTD_H 1
-
-/* Define if we have stdint.h */
-#define HAVE_STDINT_H 1
-
-#define HAVE_NANOSLEEP 1
-
-#define HAVE_TIME_H 1
+#ifdef OS_WINDOWS
+  #define HAVE_STAT 1
+  #define HAVE_UNISTD_H 0
+  #define HAVE_STDINT_H 1
+  #define HAVE_SLEEP 1
+  #define HAVE_WINDOWS_H 1
+#elif OS_UNIX
+  #define HAVE_STAT 1
+  #define HAVE_UNISTD_H 1
+  #define HAVE_STDINT_H 1
+  #define HAVE_NANOSLEEP 1
+  #define HAVE_TIME_H 1
+#endif
 
 #endif
